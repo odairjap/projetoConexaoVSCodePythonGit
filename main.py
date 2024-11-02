336cfc643c0344195063b0e7f72f9f82812516bf
odairjap <odairjap@gmail.com>
2024-11-02 12:22:15 -0300

    meu primeiro comit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de Dados")
+# Variáveis
+idade = 41
+nome = "Odair"
+
+# Print
+print(idade)
+
+# Soma
+novaIdade = idade + 1
+print(novaIdade)
+
+# Lista
+lista = [1 ,2, 3, 4, 5]
+
+# Métodos
+listaInvertida = lista.reverse()
+print(lista)
+print(listaInvertida)
