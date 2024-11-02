5dd637530ba5bf60a777e574864e59baf11ff1c2
odairjap <odairjap@gmail.com>
2024-11-02 13:30:27 -0300

    adicinando comandos para exclusão e recuperação

--- testGit.py
+++ testGit.py
@@ -1,2 +1,11 @@
 #teste exclusão e recuperação arquivos no GIT
 print("Hello World!")
+# Apagar o arguivotestGit.py
+# Conferir a alteração com $ git status
+# Será apresentado a exclusão não comitada do arquivo
+# restaurar com $ git restore testGit.py
+# deletar o arquivo
+# adicionar a exclusão com $ git add testGit.py
+# Confirmar a exclusão com $ git commit -m "excluir arquivo"
+#
+#
